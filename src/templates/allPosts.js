c1464651aa058b4221e5da44288bbd99a08132aa
gbecker99 <54383476+gbecker99@users.noreply.github.com>
2021-06-18 14:06:21 +0800

    Add files via upload

--- src/templates/allPosts.js
+++ src/templates/allPosts.js
@@ -3,9 +3,7 @@ import { graphql } from "gatsby"
 import {
   Container,
   Content,
-  ContentCard,
   FeatureImage,
-  Pagination,
   Seo,
 } from "../components"
 import { H1, P } from "../elements"
@@ -25,10 +23,10 @@ const allPosts = ({ pageContext, data }) => {
       <FeatureImage />
       <Content>
         <H1 textAlign="center" margin="0 0 1rem 0">
-          UNDER CONSTRUCTION
+          YOU... ELEVATE... 2VIBRANT
         </H1>
         <P color="dark2" textAlign="center">
-          We are busy developing a new look and awesome new content. Please return again soon.
+        UNDER CONSTRUCTION<br></br>We are busy developing a new look and some awesome new content. Please return again soon.
         </P>
       
       
